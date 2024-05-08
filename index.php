4c1cd1ba11051be2a798d178f30d543f63fb168e
rampriyadas <rampriyadas@git.com>
2024-05-09 03:15:34 +0530

    okay

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 
 require './config.php';
 
-$res = pg_query("SELECT * FROM maps");
-$data = [];
-while ($row_users = pg_fetch_object($res)) {
-
-   $data[] = $row_users;
-
-
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+   if(isset($_FILES['file']) && !empty($_FILES['file']) && is_array($_FILES['file'])){
+       $zip = new ZipArchive();
+       $zip_file_name = date("YmdHis").".zip";
+       if($zip->open($zip_file_name, ZipArchive::CREATE) === true){
+           foreach($_FILES['file']['tmp_name'] as $key => $tmpName){
+               $file_name = $_FILES['file']['name'][$key];
+               $zip->addFile($tmpName, rand(10,100).date("YmdHis").".zip");
+           }
+       
+           $zip->close();
+       }else{
+           echo "Couldn't create Zip Archive";
+       }
+   }
+   echo json_encode("Okay");
 }
-echo json_encode($data);
+?>
+<form action="/" method="post" enctype="multipart/form-data">
+        <input type="file" name="file[]">
+        <input type="file" name="file[]">
+        <input type="submit" >
+    </form>
