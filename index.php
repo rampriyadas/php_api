0bd64b83d5e8268ba20f8822ebc478ff89691195
rampriyadas <rampriyadas@git.com>
2024-05-09 13:52:15 +0530

    okay

--- index.php
+++ index.php
@@ -5,17 +5,21 @@ require './config.php';
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
    if(isset($_FILES['file']) && !empty($_FILES['file']) && is_array($_FILES['file'])){
        $zip = new ZipArchive();
-       $zip_file_name = date("YmdHis").".zip";
-       if($zip->open($zip_file_name, ZipArchive::CREATE) === true){
+       $zip_file_name = date("YmdHis");
+       if($zip->open("uploads/zip/".$zip_file_name.".zip", ZipArchive::CREATE) === true){
            foreach($_FILES['file']['tmp_name'] as $key => $tmpName){
-               $file_name = $_FILES['file']['name'][$key];
-               $zip->addFile($tmpName, rand(10,100).date("YmdHis").".zip");
+               $name = rand(10,100).date("YmdHis");
+               $zip->addFile($tmpName, $name.".zip");
+               $zip2 = new ZipArchive();
+               $zip2->open($tmpName);
+               $shapefiles_dir = 'uploads/shapefiles/'.$zip_file_name.'/'.$name; 
+               $zip2->extractTo($shapefiles_dir);
+               $zip2->close();
            }
-       
            $zip->close();
        }else{
            echo "Couldn't create Zip Archive";
-       }
+       }     
    }
    echo json_encode("Okay");
 }
